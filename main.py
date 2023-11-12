2212d7b87fbd1505533cc080cec88d513c7bc484
aalexander0422 <122483416+aalexander0422@users.noreply.github.com>
2023-11-12 16:58:56 -0600

    gg

--- main.py
+++ main.py
@@ -10,3 +10,21 @@ print(data.describe())
 # Display basic statistics for all columns
 print(data.describe(include='all'))
 
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the CSV file into a DataFrame
+data = pd.read_csv('cardata.csv')
+
+# Display histograms for numerical attributes
+numeric_columns = data.select_dtypes(include='number').columns
+
+for column in numeric_columns:
+    plt.figure(figsize=(8, 6))
+    sns.histplot(data[column], kde=True, color='skyblue')
+    plt.title(f'Histogram of {column}')
+    plt.xlabel(column)
+    plt.ylabel('Frequency')
+    plt.show()
+
