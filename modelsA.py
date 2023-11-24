b1ac005691e6c6651bce1149aecd654976d8397b
aalexander0422 <122483416+aalexander0422@users.noreply.github.com>
2023-11-23 21:41:30 -0600

    dr

--- modelsA.py
+++ modelsA.py
@@ -1,9 +1,8 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
-from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+df=pd.read_csv('cardata.csv')
 
-df = pd.read_csv("cardata.csv")
-df
+print(df.head())
